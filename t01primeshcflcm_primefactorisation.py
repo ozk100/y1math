f4e5e77cf92249eaad0255d268cc0392d884ba26
zeekay <48120720+ozk100@users.noreply.github.com>
2019-10-18 15:34:15 +0800

    Update t01primeshcflcm_primefactorisation.py

--- t01primeshcflcm_primefactorisation.py
+++ t01primeshcflcm_primefactorisation.py
@@ -1,3 +1,30 @@
 # The process of expressing a number as a product of its prime factors is called prime factorisation.
 # Express 18 as a prime factorisation.
+import sys,math
+def factorize(n):
+    def isPrime(n):
+        return not [x for x in xrange(2,int(math.sqrt(n))+1)
+                    if n%x == 0]
+    primes = []
+    candidates = xrange(2,n+1)
+    candidate = 2
+    while not primes and candidate in candidates:
+        if n%candidate == 0 and isPrime(candidate):
+            primes = primes + [candidate] + factorize(n/candidate)
+        candidate += 1            
+    return primes
 
+def condense(L):
+  prime,count,list=0,0,[]
+  for x in L:
+    if x == prime:
+      count = count + 1
+    else:
+      if prime != 0:
+        list = list + [str(prime) + '^' + str(count)]
+      prime,count=x,1
+  list = list + [str(prime) + '^' + str(count)]
+  return list
+
+print factorize(int(sys.argv[1]))
+print condense(factorize(int(sys.argv[1])))
